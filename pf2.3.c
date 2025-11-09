86028ce4df7c5bb66662e23cf7cdff01baed9f75
Mohaddisa Zehra <k250663@nu.edu.pk>
2025-11-09 23:04:08 +0500

    pf2.3.c

--- pf2.3.c
+++ pf2.3.c
@@ -2,19 +2,7 @@
 int dec , bin = 0 ,fn ;
 int arr[2][2] = {{1,3} , {2 , 5}};
 
-void dectobin(int dec){
-	int i = 0;
-	int bin[32];
-	while (dec > 0) {
-        bin[i] = dec & 1;
-        dec = dec >> 1;         
-        i++;
-    }
-printf("binary number is: ");
-for(int j = i -1 ; j>=0 ; j--){
-	 printf("%d" , bin[j]);
-}
-}
+
 void grid(){
 		printf("grid:\n");
 	for (int i =0 ; i<2 ; i++){
@@ -51,15 +39,30 @@ void query(){
 		for(int j =0 ; j<2 ; j++){
 			if(arr[i][j] == arr[posi -1][posj -1]){
 				dec = arr[i][j] ;
+int i = 0;
+	int bin[32];
+	while (dec > 0) {
+        bin[i] = dec & 1;
+        dec = dec >> 1;         
+        i++;
+    }
+printf("binary number is: ");
+for(int j = i -1 ; j>=0 ; j--){
+	 printf("%d" , bin[j]);
+}
 			}
 		}
 	}
-}
-void report(){
-	int dec[];
-	if(dec ){
-		printf("")
-	}
+	int value = arr[posi - 1][posj - 1];
+	
+	int pwr = dec & 1;                                 // Bit 0
+    int overload = (dec >> 1) & 1;                       // Bit 1
+    int maint = (dec >> 2) & 1;       // bit 2
+    
+     printf("\nREPORT ");
+    printf("Power Status: %s\n", pwr ? "ON" : "OFF");
+    printf("Overload Warning: %s\n", overload ? "OVERLOADED" : "NORMAL");
+    printf("Maintenance Required: %s\n", maint ? "YES" : "NO");
 }
 
 int main(){
@@ -73,6 +76,6 @@ grid();
 	 break;
 	 case 2:
 	query();
-	dectobin(dec);
+	
 }
 }
